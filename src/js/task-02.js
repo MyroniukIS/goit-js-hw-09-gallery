3013f6d42415616fa2ad9f01d19443a0fcec98c6
Eduard-Konovka <ed098ua@gmail.com>
2021-08-06 21:22:16 +0300

    Solving the task 02

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,1 +1,16 @@
 const ingredients = ['Картошка', 'Грибы', 'Чеснок', 'Помидоры', 'Зелень', 'Приправы']
+
+const ingredientsList = document.querySelector('#ingredients')
+
+// Пишем функцию для создания разметки списка ингридиентов
+const makeIngredientsList = elements => {
+  return elements.map(element => {
+    const listItem = document.createElement('li')
+    listItem.textContent = element
+
+    return listItem
+  })
+}
+
+const tegsArray = makeIngredientsList(ingredients)
+ingredientsList.append(...tegsArray)
