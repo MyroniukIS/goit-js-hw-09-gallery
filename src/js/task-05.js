a39aa6bb6d5fce476ff45c195ce17d71a0454ec7
Eduard-Konovka <ed098ua@gmail.com>
2021-08-07 19:42:03 +0300

    Update task-05.js

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -2,6 +2,6 @@ const formInput = document.querySelector('#name-input')
 const greetingName = document.querySelector('#name-output')
 
 formInput.addEventListener('input', event => (greetingName.textContent = event.currentTarget.value))
-formInput.addEventListener('change', event =>
-  event.currentTarget.value === '' ? (greetingName.textContent = 'незнакомец') : event.currentTarget.value,
+formInput.addEventListener('blur', event =>
+  event.currentTarget.value === '' ? (greetingName.textContent = 'незнакомец') : null,
 )
