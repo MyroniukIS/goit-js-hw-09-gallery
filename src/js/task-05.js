085e39a4f7aa5b17a1d38fccc0ce9f2bbe54dc96
Eduard-Konovka <ed098ua@gmail.com>
2021-08-12 11:31:29 +0300

    Стартовая иерархия файлов

--- src/js/task-05.js
+++
@@ -1,7 +0,0 @@
-const formInput = document.querySelector('#name-input')
-const greetingName = document.querySelector('#name-output')
-
-formInput.addEventListener('input', event => (greetingName.textContent = event.currentTarget.value))
-formInput.addEventListener('blur', event =>
-  event.currentTarget.value === '' ? (greetingName.textContent = 'незнакомец') : null,
-)
--- src/js/task-07.js
+++
@@ -1,5 +0,0 @@
-const rangeInput = document.querySelector('#font-size-control')
-const fontText = document.querySelector('#text')
-fontText.style.fontSize = '50px'
-
-rangeInput.addEventListener('input', () => (fontText.style.fontSize = `${rangeInput.valueAsNumber}px`))
