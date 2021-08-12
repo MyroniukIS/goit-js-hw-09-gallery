6e7a758fb8f7181bbf4cb442b5318dbf97b59c26
Eduard-Konovka <ed098ua@gmail.com>
2021-08-12 15:59:18 +0300

    Реализация делегирования на галерее
    Реализация делегирования на галерее `ul.js-gallery` и получение `url` большого изображения

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,4 @@
+//- Создание и рендер разметки по массиву данных `galleryItems` из `app.js` и предоставленному шаблону
 import galleryItems from './app.js'
 
 const gallery = document.querySelector('.js-gallery')
@@ -19,3 +20,29 @@ gallery.insertAdjacentHTML(
     )
     .join(''),
 )
+
+//- Реализация делегирования на галерее `ul.js-gallery` и получение `url` большого изображения
+gallery.addEventListener('click', handleNavClick)
+
+function handleNavClick(event) {
+  event.preventDefault()
+
+  const target = event.target
+
+  if (target.nodeName !== 'IMG') return
+
+  getLargerImageLink(target)
+}
+
+function getLargerImageLink(activeLightbox) {
+  const currentLargeImage = document.querySelector('.lightbox__image')
+
+  currentLargeImage.setAttribute('src', activeLightbox.dataset.source)
+  currentLargeImage.setAttribute('alt', activeLightbox.alt)
+}
+
+//- Открытие модального окна по клику на элементе галереи.
+//- Подмена значения атрибута `src` элемента `img.lightbox__image`.
+//- Закрытие модального окна по клику на кнопку `button[data-action="close-lightbox"]`.
+//- Очистка значения атрибута `src` элемента `img.lightbox__image`. Это необходимо для того, чтобы при следующем открытии
+//  модального окна, пока грузится изображение, мы не видели предыдущее.
