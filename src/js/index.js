b27c0960155f58d15a98b9197e4a49d6c1d6d81a
Eduard-Konovka <ed098ua@gmail.com>
2021-08-12 18:50:56 +0300

    Открытие модального окна
    Открытие модального окна по клику на элементе галереи

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-//- Создание и рендер разметки по массиву данных `galleryItems` из `app.js` и предоставленному шаблону
+//- Создание и рендер разметки по массиву данных `galleryItems` из `app.js` и предоставленному шаблону.
 import galleryItems from './app.js'
 
 const gallery = document.querySelector('.js-gallery')
@@ -21,7 +21,7 @@ gallery.insertAdjacentHTML(
     .join(''),
 )
 
-//- Реализация делегирования на галерее `ul.js-gallery` и получение `url` большого изображения
+//- Реализация делегирования на галерее `ul.js-gallery` и получение `url` большого изображения.
 gallery.addEventListener('click', handleNavClick)
 
 function handleNavClick(event) {
@@ -32,16 +32,23 @@ function handleNavClick(event) {
   if (target.nodeName !== 'IMG') return
 
   getLargerImageLink(target)
+  toOpenLightbox()
 }
 
-function getLargerImageLink(activeLightbox) {
+function getLargerImageLink(targetImage) {
   const currentLargeImage = document.querySelector('.lightbox__image')
 
-  currentLargeImage.setAttribute('src', activeLightbox.dataset.source)
-  currentLargeImage.setAttribute('alt', activeLightbox.alt)
+  currentLargeImage.setAttribute('src', targetImage.dataset.source)
+  currentLargeImage.setAttribute('alt', targetImage.alt)
 }
 
 //- Открытие модального окна по клику на элементе галереи.
+function toOpenLightbox() {
+  const lightbox = document.querySelector('.js-lightbox')
+
+  lightbox.classList.add('is-open')
+}
+
 //- Подмена значения атрибута `src` элемента `img.lightbox__image`.
 //- Закрытие модального окна по клику на кнопку `button[data-action="close-lightbox"]`.
 //- Очистка значения атрибута `src` элемента `img.lightbox__image`. Это необходимо для того, чтобы при следующем открытии
