e66d4789aeb393be583e91d5aeaa5276e53ce67a
Myroniuk Ilona <knopkakyrs@gmail.com>
2021-08-22 11:45:15 +0300

    create elements

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 //- Создание и рендер разметки по массиву данных `galleryItems` из `db.js` и предоставленному шаблону.
-import { gallery } from './js/rendering-markup'
+import { galleryUl } from './js/rendering-markup'
 
 //- Реализация делегирования на галерее `ul.js-gallery` и получение `url` большого изображения.
 // import { handleNavClick } from './js/to-delegate'
 
-gallery.addEventListener('click', handleNavClick)
+galleryUl.addEventListener('click', handleNavClick)
 
 
 
--- src/js/rendering-markup.js
+++ src/js/rendering-markup.js
@@ -1,23 +1,32 @@
 import { galleryItems } from './app'
 
-//- Создание и рендер разметки по массиву данных `galleryItems` из `db.js` и предоставленному шаблону.
-export const gallery = document.querySelector('.js-gallery')
+//- Создание и рендер разметки по массиву данных `galleryItems` из `app.js` и предоставленному шаблону.
+export const galleryUl = document.querySelector('.js-gallery');
 
-gallery.insertAdjacentHTML(
-  'beforeend',
-  galleryItems
-    .map(
-      image =>
-        `<li class="gallery__item">
-          <a class="gallery__link" href="${image.original}">
-            <img
-              class="gallery__image"
-              src="${image.preview}"
-              data-source="${image.original}"
-              alt="${image.description}"
-            />
-          </a>
-        </li>`,
-    )
-    .join(''),
-)
+const elementsGallery = createGalleryElements(galleryItems);
+
+galleryUl.insertAdjacentHTML('beforeend', elementsGallery)
+
+function createGalleryElements(galleryItems) {
+  return galleryItems.map(({ preview, original, description }) => {
+    return `
+    <li class="gallery__item">
+  <a
+    class="gallery__link"
+    href="${original}"
+  >
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+  })
+    .join('');
+  
+};   
+  
+  
+// galleryUl.addEventListener('click', onGalerryContainerClick);
