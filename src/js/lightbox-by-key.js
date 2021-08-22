4d0dca449dad50621b99e881f20063d81d67c28d
Myroniuk Ilona <knopkakyrs@gmail.com>
2021-08-22 14:08:41 +0300

    delete

--- src/js/lightbox-by-key.js
+++
@@ -1,8 +0,0 @@
- import { toCloseLightbox } from './to-close-lightbox'
-
-
-// //- Управление модального окна по нажатию клавиш
- export function lightboxByKey(event) {
-   event.code === 'Escape' ? toCloseLightbox() : null
- 
- }
--- src/js/reset-larger-image-link.js
+++
@@ -1,8 +0,0 @@
-// import { currentLargeImage } from './current-large-image'
-
-// //- Очистка значения атрибута `src` элемента `img.lightbox__image`. Это необходимо для того, чтобы при следующем открытии
-// //  модального окна, пока грузится изображение, мы не видели предыдущее.
-export function resetLargerImageLink() {
-  currentLargeImage.setAttribute('src', '')
-  currentLargeImage.setAttribute('alt', '')
-}
